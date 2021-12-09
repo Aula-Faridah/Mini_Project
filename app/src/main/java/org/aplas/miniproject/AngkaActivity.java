a3ed09668848c4cd835e0935f97a676c33777a75
Aula-Faridah <aulafaridah@gmail.com>
2021-12-09 09:26:52 +0700

    Commit : angka,huruf,warna java dan xml

--- app/src/main/java/org/aplas/miniproject/AngkaActivity.java
+++ app/src/main/java/org/aplas/miniproject/AngkaActivity.java
@@ -2,13 +2,24 @@ package org.aplas.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AngkaActivity extends AppCompatActivity {
-
+    private ImageButton buttonPrev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_angka);
+        buttonPrev = (ImageButton) findViewById(R.id.buttonPrev);
+        buttonPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+            }
+        });
     }
 }
--- app/src/main/java/org/aplas/miniproject/WarnaActivity.java
+++ app/src/main/java/org/aplas/miniproject/WarnaActivity.java
@@ -2,13 +2,24 @@ package org.aplas.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class WarnaActivity extends AppCompatActivity {
-
+    private ImageButton buttonPrev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warna);
+        buttonPrev = (ImageButton) findViewById(R.id.buttonPrev);
+        buttonPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+            }
+        });
     }
 }
